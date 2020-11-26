af702d715c0ff49127661210f30bf4ead4f5a5f4
Marlon Lückert <m.lueckert@me.com>
2020-11-26 16:56:38 +0100

    clean up

--- serverless/handler.js
+++ serverless/handler.js
@@ -39,7 +39,4 @@ module.exports.general = async event => {
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
