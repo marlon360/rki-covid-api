fc9459333131668ef471e6e898f33f38c1cb7514
Marlon Lückert <m.lueckert@me.com>
2021-01-04 15:33:18 +0100

    added quote and indication to vacciantions

--- src/data-requests/vaccination.ts
+++ src/data-requests/vaccination.ts
@@ -6,11 +6,27 @@ import { getStateAbbreviationByName } from '../utils';
 export interface VaccinationCoverage {
     vaccinated: number,
     delta: number,
+    vaccinatedPer1k: number,
+    quote: number,
+    indication: {
+        age: number,
+        job: number,
+        medical: number,
+        nursingHome: number
+    },
     states: {
         [abbreviation: string]: {
             name: string,
             vaccinated: number,
-            delta: number
+            delta: number,
+            vaccinatedPer1k: number,
+            quote: number,
+            indication: {
+                age: number,
+                job: number,
+                medical: number,
+                nursingHome: number
+            }
         }
     } 
 }
@@ -39,20 +55,44 @@ export async function getVaccinationCoverage(): Promise<ResponseData<Vaccination
     const coverage: VaccinationCoverage = {
         vaccinated: 0,
         delta: 0,
+        vaccinatedPer1k: 0,
+        quote: 0,
+        indication: {
+            age: 0,
+            job: 0,
+            medical: 0,
+            nursingHome: 0
+        },
         states: {}
     }
 
     for (const entry of json) {
-        if (Object.keys(entry).length == 7) {
+        if (Object.keys(entry).length > 4) {
             if (entry.Bundesland == "Gesamt") {
                 coverage.vaccinated = entry['Impfungen kumulativ']
-                coverage.delta = entry['Differenz zum Vortag']
+                coverage.delta = entry['Differenz zum Vortag'],
+                coverage.vaccinatedPer1k = entry['Impfungen pro 1.000 Einwohner'],
+                coverage.quote = coverage.vaccinated / (coverage.vaccinated / coverage.vaccinatedPer1k * 1000)
+                coverage.indication = {
+                    age: entry['Indikation nach Alter*'],
+                    job: entry['Berufliche Indikation*'],
+                    medical: entry['Medizinische Indikation*'],
+                    nursingHome: entry['Pflegeheim-bewohnerIn*']
+                }
             } else {
                 const abbreviation = getStateAbbreviationByName(entry.Bundesland)
                 coverage.states[abbreviation] = {
                     name: entry.Bundesland,
                     vaccinated: entry['Impfungen kumulativ'],
-                    delta: entry['Differenz zum Vortag']
+                    delta: entry['Differenz zum Vortag'],
+                    vaccinatedPer1k: entry['Impfungen pro 1.000 Einwohner'],
+                    quote: entry['Impfungen kumulativ'] / (entry['Impfungen kumulativ'] / entry['Impfungen pro 1.000 Einwohner'] * 1000),
+                    indication: {
+                        age: entry['Indikation nach Alter*'],
+                        job: entry['Berufliche Indikation*'],
+                        medical: entry['Medizinische Indikation*'],
+                        nursingHome: entry['Pflegeheim-bewohnerIn*']
+                    }
                 }
             }
         }
