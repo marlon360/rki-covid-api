352af089abf4b6ec83d7ec6d1e0cf6a8bdbd016e
Marlon Lückert <m.lueckert@me.com>
2021-01-18 15:00:54 +0100

    get general from v2

--- server/api/general.js
+++ server/api/general.js
@@ -1,66 +1,20 @@
 'use strict';
 
+const axios = require('axios');
+
 module.exports.general = async (req, res) => {
   try {
-    const db = req.app.locals.database;
-    let dCollection = db.collection("general");
-    let result;
 
-    // test if an entry for today exists
-    const latestEntries = await dCollection.find().sort({ _id: -1 }).limit(2).toArray();
+    const response = await axios.get("https://api.corona-zahlen.org/germany");
+    const apidata = response.data;
 
-    if (latestEntries.length == 0) {
-      throw 'No entries in the database!';
-    }
-
-    if (latestEntries.length > 0) {
-
-      const todayData = latestEntries[0];
-      let yesterdayData = null;
-
-      // check if one day difference
-      if (latestEntries.length == 2) {
-        const today = new Date(latestEntries[0].date);
-        const yesterday = new Date(latestEntries[1].date);
-
-        const oneDayDifference = 1000 * 60 * 60 * 24;
-        if (today - yesterday == oneDayDifference) {
-          yesterdayData = latestEntries[1]
-        }
+    res.json({
+      ...apidata,
+      lastUpdate: (new Date(apidata.meta.lastUpdate)).toLocaleString("de-DE"),
+      difference: {
+        ...apidata.delta
       }
-
-      let difference;
-      if (yesterdayData != null) {
-        difference = {
-          recovered: todayData.recovered - yesterdayData.recovered,
-          cases: todayData.cases - yesterdayData.cases,
-          deaths: todayData.deaths - yesterdayData.deaths,
-          weekIncidence: todayData.weekIncidence - yesterdayData.weekIncidence,
-          casesPerWeek: todayData.casesPerWeek - yesterdayData.casesPerWeek,
-          casesPer100k: todayData.casesPer100k - yesterdayData.casesPer100k
-        }
-      } else {
-        difference = {
-          recovered: null,
-          cases: null,
-          deaths: null,
-          weekIncidence: null,
-          casesPerWeek: null,
-          casesPer100k: null
-        }
-      }
-
-      // clear unimportant keys
-      delete todayData.date;
-      delete todayData._id;
-
-      result = {
-        ...todayData,
-        difference
-      }
-    }
-
-    res.json({...result})
+    })
 
   } catch (e) {
     res.statusCode = 500;
