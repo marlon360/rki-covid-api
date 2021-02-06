281ed7f68d31d16a721cade27756324a295c6bfa
Peter <peter@markewitz.org>
2021-02-06 08:06:26 +0100

    Revert "Fix Colortable (gap between 5 and 15 and change colors to "RKI-Like""

--- src/configuration/colors.ts
+++ src/configuration/colors.ts
@@ -3,42 +3,57 @@ export const weekIncidenceColorRanges = {
         {
             min: 0,
             max: 1,
-            color: "rgb(173, 173, 173)"
+            color: "#2D81B8"
         },
         {
             min: 1,
             max: 5,
-            color: "rgb(255, 252, 205)"
+            color: "#7FD38D"
         },
         {
-            min: 5,
+            min: 15,
             max: 25,
-            color: "rgb(255, 243, 128)"
+            color: "#FEFFB1"
         },
         {
             min: 25,
+            max: 35,
+            color: "#FECA81"
+        },
+        {
+            min: 35,
             max: 50,
-            color: "rgb(255, 181, 52)"
+            color: "#F08A4B"
         },
         {
             min: 50,
             max: 100,
-            color: "rgb(212, 54, 36)"
+            color: "#EB1A1D"
         },
         {
             min: 100,
-            max: 250,
-            color: "rgb(149, 18, 20)"
+            max: 200,
+            color: "#AB1316"
         },
         {
-            min: 250,
+            min: 200,
+            max: 350,
+            color: "#B374DD"
+        },
+        {
+            min: 350,
             max: 500,
-            color: "rgb(103, 18, 18)"
+            color: "#5B189B"
         },
         {
             min: 500,
+            max: 1000,
+            color: "#543D35"
+        },
+        {
+            min: 1000,
             max: Infinity,
-            color: "rgb(221, 0, 133)"
+            color: "#020003"
         }
     ]
 }
