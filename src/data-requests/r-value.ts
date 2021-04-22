c4c8c75d4b2fb3a3569e6a4826886f5d669aa6ae
Patrick Haußmann <git@patrickhaussmann.de>
2021-04-22 09:19:38 +0200

    Fix #185
    RKI renamed the column in the Excel file

--- src/data-requests/r-value.ts
+++ src/data-requests/r-value.ts
@@ -9,7 +9,7 @@ function parseRValue(data: ArrayBuffer): { r: number, date: Date } | null {
 
     const latestEntry = json[json.length - 1];
     const dateString = latestEntry["Datum des Erkrankungsbeginns"];
-    let rValue = latestEntry["Punktschätzer des 4-Tage R-Wertes"] || latestEntry["Punktschätzer der 4-Tage R-Wert"];
+    let rValue = latestEntry["Punktschätzer des 4-Tage-R-Wertes"] || latestEntry["Punktschätzer der 4-Tage R-Wert"];
 
     if (typeof rValue === 'string' || rValue instanceof String) {
         rValue = parseFloat(rValue.replace(",", "."))
