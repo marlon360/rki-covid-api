a87f0506c546b94a3187bfa7072a749712f86ecb
Laurenz Grote <laurenzfg@laurenzgrote.de>
2021-05-17 10:33:09 +0200

    Implement webhook support

---
+++ src/configuration/webhooks.ts
@@ -0,0 +1,9 @@
+export const webhooks = {
+    hooks: [
+        // Example: Call localhost:8000/newvac whenever new vaccination data is available
+        // {
+        //     on: "/vaccinations",
+        //     call: "http://localhost:8000/newvac"
+        // },
+    ]
+}
--- src/webhook-middleware.ts
+++ src/webhook-middleware.ts
@@ -1,13 +1,42 @@
 import { IResponseMeta } from './responses/meta'
 import { Request } from 'express'
+import { promisify } from 'util'
+import { webhooks } from './configuration/webhooks'
+import axios from "axios";
+
+const redis = require("redis");
+const client = redis.createClient();
+
+// Make a Promise based Set for redis
+const setAsync = promisify(client.set).bind(client);
+
+function callhooks (of: string) {
+    webhooks.hooks.forEach(element => {
+        if (element.on === of) {
+            axios.get(element.call).catch((error) => {
+                console.log("Since " + element.on + " hook was triggered, we tried to call " + element.call +
+                " but received " + error);   
+            });
+        }
+    });
+}
 
 const webhookMiddleware = (data: IResponseMeta, req: Request) => {
     const route_name = req.path;
     const last_update = data.meta.lastUpdate.getTime();
 
     // After returning the request, check if we have new data on the relevant endpoint
-    setImmediate(() => {
-        console.log(route_name + ": " + last_update);
+    setImmediate(async () => {
+        try {
+            const res = await setAsync ("webhook:" + route_name, last_update, "GET");
+            // res now contains the last update which was processed by the hook
+            if (res < last_update) {
+                // file got updated, let's call all the hooks
+                callhooks(route_name);
+            }
+        } catch (error) {
+            console.log("Webhook Module has redis issue: " + error);
+        }
     });
     
 };
