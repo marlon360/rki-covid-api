89f99210cbd482181ca8965d69af6b8037dc8a37
Peter <peter@markewitz.org>
2021-05-26 08:26:34 +0200

    Add 7 day r value

--- src/data-requests/response-data.ts
+++ src/data-requests/response-data.ts
@@ -2,3 +2,10 @@ export interface ResponseData<T> {
   data: T;
   lastUpdate: Date;
 }
+
+export interface Response_rData<> {
+  data_r4: number;
+  lastUpdate_r4: Date;
+  data_r7: number;
+  lastUpdate_r7: Date;
+}
