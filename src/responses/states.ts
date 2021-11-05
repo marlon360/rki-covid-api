ed81905834be4fa407afc8e84a7910c20af42bf3
Peter <peter@markewitz.org>
2021-11-06 00:30:23 +0100

    cleanup function getStateById
    i had slightly changed this function for my hospitalization solution. No longer needed!

--- src/responses/states.ts
+++ src/responses/states.ts
@@ -73,14 +73,9 @@ export async function StatesResponse(
   function getStateById(
     data: ResponseData<any[]>,
     id: number,
-    ageGroup?: string
   ): any | null {
     for (const state of data.data) {
-      if (ageGroup === undefined) {
-        if (state.id == id) return state;
-      } else {
-        if (state.id == id && state.ageGroup == ageGroup) return state;
-      }
+      if (state.id == id) return state;
     }
     return null;
   }
