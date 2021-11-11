72d587cd9d715a592c198563a8e32bbef6e9251e
Peter Markewitz <peter@markewitz.org>
2021-11-11 22:06:35 +0100

    color and range definitions should be only at one point (#318)
    * color and range definitions should be only at one point
    
    * looping through the ranges and adding a <text> and <rect>
    
    * delete automatically added import line
    
    * rename variable and delete console.log line
    
    * add dynamically placed legend
    
    * add some comments
    
    * removed absolut positioning (x=92 y=189) from text 'Marlon Lückert'
    
    * clean up, class for ColorRange
    
    Co-authored-by: Marlon Lückert <m.lueckert@me.com>

--- src/configuration/colors.ts
+++ src/configuration/colors.ts
@@ -1,59 +1,109 @@
-export const weekIncidenceColorRanges = {
-  ranges: [
-    {
-      min: 0,
-      max: 1,
-      color: "#D1D1D1",
-    },
-    {
-      min: 1,
-      max: 15,
-      color: "#7FD38D",
-    },
-    {
-      min: 15,
-      max: 25,
-      color: "#FEFFB1",
-    },
-    {
-      min: 25,
-      max: 35,
-      color: "#FECA81",
-    },
-    {
-      min: 35,
-      max: 50,
-      color: "#F08A4B",
-    },
-    {
-      min: 50,
-      max: 100,
-      color: "#EB1A1D",
-    },
-    {
-      min: 100,
-      max: 200,
-      color: "#AB1316",
-    },
-    {
-      min: 200,
-      max: 350,
-      color: "#B374DD",
-    },
-    {
-      min: 350,
-      max: 500,
-      color: "#5B189B",
-    },
-    {
-      min: 500,
-      max: 1000,
-      color: "#543D35",
-    },
-    {
-      min: 1000,
-      max: Infinity,
-      color: "#020003",
-    },
-  ],
-};
+export class ColorRange {
+  min: number; // minimum number of range (number not included >)
+  max: number; // maximum number of range (number included <=)
+  color: string; // the color on the map
+  label?: string; // the label of this range on the legend
+  compareFn?: (value: number, range: ColorRange) => boolean; // how to evaluate if a value is in the range
+
+  constructor(options: {
+    min: number;
+    max: number;
+    color: string;
+    label?: string;
+    compareFn?: (value: number, range: ColorRange) => boolean;
+  }) {
+    this.min = options.min;
+    this.max = options.max;
+    this.color = options.color;
+    this.label = options.label;
+    this.compareFn = options.compareFn;
+  }
+
+  // this string is displayed in the legend (can be overwritten with the label option)
+  toString() {
+    if (this.label) {
+      return this.label;
+    } else {
+      if (this.max === Infinity) {
+        return `> ${this.min}`;
+      } else {
+        return `> ${this.min} - ${this.max}`;
+      }
+    }
+  }
+
+  // this function is used to evaluate if a value is inside this range
+  // can be overwritten with the compareFn option e.g. for a range that needs an exact match of a number
+  isValueInRange(value: number): boolean {
+    if (this.compareFn) {
+      return this.compareFn(value, this);
+    } else {
+      return value > this.min && value <= this.max;
+    }
+  }
+}
+
+export const weekIncidenceColorRanges: ColorRange[] = [
+  new ColorRange({
+    min: 0,
+    max: 0,
+    color: "#CDCDCD",
+    compareFn: (value: number, range: ColorRange) => value === range.min,
+    label: "keine Fälle übermittelt",
+  }),
+  new ColorRange({
+    min: 0,
+    max: 1,
+    color: "#88F89B",
+  }),
+  new ColorRange({
+    min: 1,
+    max: 15,
+    color: "#7FD38D",
+  }),
+  new ColorRange({
+    min: 15,
+    max: 25,
+    color: "#FEFFB1",
+  }),
+  new ColorRange({
+    min: 25,
+    max: 35,
+    color: "#FECA81",
+  }),
+  new ColorRange({
+    min: 35,
+    max: 50,
+    color: "#F08A4B",
+  }),
+  new ColorRange({
+    min: 50,
+    max: 100,
+    color: "#EB1A1D",
+  }),
+  new ColorRange({
+    min: 100,
+    max: 200,
+    color: "#AB1316",
+  }),
+  new ColorRange({
+    min: 200,
+    max: 350,
+    color: "#B374DD",
+  }),
+  new ColorRange({
+    min: 350,
+    max: 500,
+    color: "#5B189B",
+  }),
+  new ColorRange({
+    min: 500,
+    max: 1000,
+    color: "#543D35",
+  }),
+  new ColorRange({
+    min: 1000,
+    max: Infinity,
+    color: "#020003",
+  }),
+];
--- src/responses/map.ts
+++ src/responses/map.ts
@@ -3,7 +3,7 @@ import DistrictsMap from "../maps/districts.json";
 import StatesMap from "../maps/states.json";
 import { getDistrictsData } from "../data-requests/districts";
 import { getStatesData } from "../data-requests/states";
-import { weekIncidenceColorRanges } from "../configuration/colors";
+import { ColorRange, weekIncidenceColorRanges } from "../configuration/colors";
 import sharp from "sharp";
 
 export async function DistrictsMapResponse() {
@@ -58,7 +58,11 @@ export async function DistrictsLegendMapResponse() {
   const svgBuffer = Buffer.from(stringify(mapData));
 
   return sharp(
-    getMapBackground("7-Tage-Inzidenz der Landkreise", districtsData.lastUpdate)
+    getMapBackground(
+      "7-Tage-Inzidenz der Landkreise",
+      districtsData.lastUpdate,
+      weekIncidenceColorRanges
+    )
   )
     .composite([{ input: svgBuffer, top: 100, left: 180 }])
     .png({ quality: 75 })
@@ -121,7 +125,11 @@ export async function StatesLegendMapResponse() {
   const svgBuffer = Buffer.from(stringify(mapData));
 
   return sharp(
-    getMapBackground("7-Tage-Inzidenz der Bundesländer", statesData.lastUpdate)
+    getMapBackground(
+      "7-Tage-Inzidenz der Bundesländer",
+      statesData.lastUpdate,
+      weekIncidenceColorRanges
+    )
   )
     .composite([{ input: svgBuffer, top: 100, left: 180 }])
     .png({ quality: 75 })
@@ -130,91 +138,68 @@ export async function StatesLegendMapResponse() {
 
 export function IncidenceColorsResponse() {
   return {
-    incidentRanges: weekIncidenceColorRanges.ranges,
+    incidentRanges: weekIncidenceColorRanges,
   };
 }
 
 function getColorForWeekIncidence(weekIncidence: number): string {
-  for (const range of weekIncidenceColorRanges.ranges) {
-    if (weekIncidence >= range.min && weekIncidence < range.max) {
+  for (const range of weekIncidenceColorRanges) {
+    if (range.isValueInRange(weekIncidence)) {
       return range.color;
     }
   }
   return "#FFF";
 }
 
-function getMapBackground(headline: string, lastUpdate: Date): Buffer {
-  return Buffer.from(`
+function getMapBackground(
+  headline: string,
+  lastUpdate: Date,
+  ranges: ColorRange[]
+): Buffer {
+  const border = 32; // for the legend, left and down
+  const yStartPosition = 1000 - border * 2; // start position from the bottom, add new ranges above
+  const lastUpdateLocaleString = lastUpdate.toLocaleDateString("de-DE", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  }); // localized lastUpdate string
+  // the first part of svg
+  let svg = `
     <svg width="850px" height="1000px" viewBox="0 0 850 1000" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
       <g id="Artboard" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-          <rect fill="#F4F8FB" x="0" y="0" width="850" height="1000"></rect>
-          <text id="7-Tage-Inzidenz-der" font-family="Helvetica-Bold, Helvetica" font-size="42" font-weight="bold" fill="#010501">
-              <tspan x="41" y="68">${headline}</tspan>
-          </text>
-          <text id="Stand:-22.11.2021" font-family="Helvetica" font-size="22" font-weight="normal" fill="#010501">
-              <tspan x="41" y="103">Stand: ${lastUpdate.toLocaleDateString(
-                "de-DE",
-                { year: "numeric", month: "2-digit", day: "2-digit" }
-              )}</tspan>
-          </text>
-          <g transform="translate(32.000000, 536.000000)">
-              <rect fill="#D1D1D1" x="0" y="0" width="30" height="30"></rect>
-              <rect fill="#7FD38D" x="0" y="40" width="30" height="30"></rect>
-              <rect fill="#FEFFB1" x="0" y="80" width="30" height="30"></rect>
-              <rect fill="#FECA81" x="0" y="120" width="30" height="30"></rect>
-              <rect fill="#F08A4B" x="0" y="160" width="30" height="30"></rect>
-              <rect fill="#EB1A1D" x="0" y="200" width="30" height="30"></rect>
-              <rect fill="#AB1316" x="0" y="240" width="30" height="30"></rect>
-              <rect fill="#B374DD" x="0" y="280" width="30" height="30"></rect>
-              <rect fill="#5B189B" x="0" y="320" width="30" height="30"></rect>
-              <rect fill="#543D35" x="0" y="360" width="30" height="30"></rect>
-              <rect fill="#020003" x="0" y="400" width="30" height="30"></rect>
-              <text font-family="Helvetica" font-size="16" font-weight="normal" fill="#010501">
-                  <tspan x="48" y="19">&lt; 1</tspan>
-              </text>
-              <text font-family="Helvetica" font-size="16" font-weight="normal" fill="#010501">
-                  <tspan x="48" y="59">1 - 15</tspan>
-              </text>
-              <text font-family="Helvetica" font-size="16" font-weight="normal" fill="#010501">
-                  <tspan x="48" y="99">16 - 25</tspan>
-              </text>
-              <text font-family="Helvetica" font-size="16" font-weight="normal" fill="#010501">
-                  <tspan x="48" y="145">26 - 35</tspan>
-              </text>
-              <text font-family="Helvetica" font-size="16" font-weight="normal" fill="#010501">
-                  <tspan x="48" y="182">36 - 50</tspan>
-              </text>
-              <text font-family="Helvetica" font-size="16" font-weight="normal" fill="#010501">
-                  <tspan x="48" y="223">51 - 100</tspan>
-              </text>
-              <text font-family="Helvetica" font-size="16" font-weight="normal" fill="#010501">
-                  <tspan x="48" y="262">101 - 200</tspan>
-              </text>
-              <text font-family="Helvetica" font-size="16" font-weight="normal" fill="#010501">
-                  <tspan x="48" y="302">201 - 350</tspan>
-              </text>
-              <text font-family="Helvetica" font-size="16" font-weight="normal" fill="#010501">
-                  <tspan x="48" y="421">&gt; 1000</tspan>
-              </text>
-              <text font-family="Helvetica" font-size="16" font-weight="normal" fill="#010501">
-                  <tspan x="48" y="381">501 - 1000</tspan>
-              </text>
-              <text font-family="Helvetica" font-size="16" font-weight="normal" fill="#010501">
-                  <tspan x="48" y="341">351 - 500</tspan>
-              </text>
+        <rect fill="#F4F8FB" x="0" y="0" width="850" height="1000"></rect>
+        <text id="7-Tage-Inzidenz-der" font-family="Helvetica-Bold, Helvetica" font-size="42" font-weight="bold" fill="#010501">
+          <tspan x="41" y="68">${headline}</tspan>
+        </text>
+        <text id="Stand:-22.11.2021" font-family="Helvetica" font-size="22" font-weight="normal" fill="#010501">
+          <tspan x="41" y="103">Stand: ${lastUpdateLocaleString}</tspan>
+        </text>
+        <g id="Legend" transform="translate(${border}, 0)">
+        ${ranges.map((range, index) => {
+          return `
+          <g transform="translate(0, ${yStartPosition - index * 40})">
+            <rect fill="${
+              range.color
+            }" x="0" y="0" width="30" height="30"></rect>
+            <text x="48" y="20" font-family="Helvetica" font-size="16" font-weight="normal" fill="#010501">
+              <tspan>${range.toString()}</tspan>
+            </text>
           </g>
-          <rect id="Rectangle" fill="#A2D4FA" opacity="0.218688965" x="0" y="158" width="260" height="70"></rect>
-          <text id="Quelle:-Robert-Koch-" font-family="Helvetica" font-size="10" font-weight="normal" fill="#010501">
-              <tspan x="576" y="987">Quelle: Robert Koch-Institut (https://api.corona-zahlen.org)</tspan>
-          </text>
-          <text font-family="Helvetica" font-size="16" font-weight="normal" fill="#243645">
-              <tspan x="15" y="189">Grafik von</tspan>
-              <tspan x="92" y="189" font-family="Helvetica-Bold, Helvetica" font-weight="bold"> Marlon Lückert</tspan>
-          </text>
-          <text font-family="Helvetica-Bold, Helvetica" font-size="16" font-weight="bold" fill="#243645">
-              <tspan x="15" y="211">https://api.corona-zahlen.org</tspan>
-          </text>
+          `;
+        })}
+        </g>
+        <rect id="Rectangle" fill="#A2D4FA" opacity="0.218688965" x="0" y="158" width="260" height="70"></rect>
+        <text id="Quelle:-Robert-Koch-" font-family="Helvetica" font-size="10" font-weight="normal" fill="#010501">
+          <tspan x="576" y="987">Quelle: Robert Koch-Institut (https://api.corona-zahlen.org)</tspan>
+        </text>
+        <text font-family="Helvetica" font-size="16" font-weight="normal" fill="#243645">
+          <tspan x="15" y="189">Grafik von</tspan>
+          <tspan font-family="Helvetica-Bold, Helvetica" font-weight="bold"> Marlon Lückert</tspan>
+        </text>
+        <text font-family="Helvetica-Bold, Helvetica" font-size="16" font-weight="bold" fill="#243645">
+          <tspan x="15" y="211">https://api.corona-zahlen.org</tspan>
+        </text>
       </g>
-  </svg>
-  `);
+    </svg>`;
+  return Buffer.from(svg);
 }
