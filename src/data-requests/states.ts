32e63da4658f799ac7e4a9041e9c61526a262a7a
Peter Markewitz <peter@markewitz.org>
2021-11-15 16:06:21 +0100

    fix states date

--- src/data-requests/states.ts
+++ src/data-requests/states.ts
@@ -34,7 +34,9 @@ export async function getStatesData(): Promise<ResponseData<IStateData[]>> {
   });
   return {
     data: states,
-    lastUpdate: new Date(data.features[0].attributes.Aktualisierung),
+    lastUpdate: new Date(
+      data.features[0].attributes.Aktualisierung + 60 * 60 * 1000
+    ),
   };
 }
 
