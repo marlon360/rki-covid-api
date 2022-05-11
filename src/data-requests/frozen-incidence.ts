1c790ee591266965b61693ddaff35a863873c551
Peter Markewitz <peter@markewitz.org>
2022-05-11 04:22:57 +0000

    add the dates since last monday for frozen incidence history modified:   src/data-requests/frozen-incidence.ts

--- src/data-requests/frozen-incidence.ts
+++ src/data-requests/frozen-incidence.ts
@@ -1,6 +1,13 @@
 import axios from "axios";
 import XLSX from "xlsx";
-import { getDateBefore, getStateAbbreviationByName, RKIError } from "../utils";
+import {
+  AddDaysToDate,
+  getDateBefore,
+  getDayDifference,
+  getStateAbbreviationByName,
+  getStateIdByName,
+  RKIError,
+} from "../utils";
 import { ResponseData } from "./response-data";
 
 function getDateFromString(dateString: string): Date {
@@ -45,9 +52,6 @@ async function getDistrictsFrozenIncidenceHistoryArchive(): Promise<
   // table starts in row 5 (parameter is zero indexed)
   const json = XLSX.utils.sheet_to_json(sheet, { range: 4 });
 
-  // date is in cell A2
-  const lastUpdate = new Date(response.headers["last-modified"]);
-
   let districts = json
     .filter((district) => !!district["NR"])
     .map((district) => {
@@ -90,8 +94,7 @@ export async function getDistrictsFrozenIncidenceHistory(
   // table starts in row 5 (parameter is zero indexed)
   const json = XLSX.utils.sheet_to_json(sheet, { range: 4 });
 
-  // date is in cell A2
-  const lastUpdate = new Date(response.headers["last-modified"]);
+  let lastUpdate = new Date(response.headers["last-modified"]);
 
   let districts = json.map((district) => {
     const name = district["LK"];
@@ -108,7 +111,7 @@ export async function getDistrictsFrozenIncidenceHistory(
       history.push({ weekIncidence: district[dateKey], date });
     });
 
-    if (days != null) {
+    if (days) {
       const reference_date = new Date(getDateBefore(days));
       history = history.filter((element) => element.date > reference_date);
     }
@@ -116,8 +119,72 @@ export async function getDistrictsFrozenIncidenceHistory(
     return { ags, name, history };
   });
 
-  if (ags != null) {
+  if (ags) {
     districts = districts.filter((district) => district.ags === ags);
+  }
+
+  // The Excel sheet with fixed incidence data is only updated on mondays
+  // check witch date is the last date in history
+  const lastDate: Date =
+    districts[0].history[districts[0].history.length - 1].date;
+  const today: Date = new Date(new Date().setHours(0, 0, 0));
+  // get lastUpdate from github
+  const lastGithubDate = await axios
+    .get(
+      `https://raw.githubusercontent.com/Rubber1Duck/RD_RKI_COVID19_DATA/master/Fallzahlen/RKI_COVID19_meta.json`
+    )
+    .then((response) => {
+      return new Date(response.data.modified);
+    });
+  // if lastDate < today and lastDate <= lastGithubDate get the missing dates from github
+  // the gihub data is updated by github actions daily, triggert at ~ 1:58 GMT, running ?
+  let missingDatesData = [];
+  if (
+    lastDate.getTime() < today.getTime() &&
+    lastDate.getTime() <= lastGithubDate.getTime()
+  ) {
+    lastUpdate = lastGithubDate;
+    const numberOfmissingDays = getDayDifference(today, lastDate) - 1;
+    const numberOfmissingDaysMax =
+      getDayDifference(lastGithubDate, lastDate) - 1;
+    const maxNumberOfDays = Math.min(
+      numberOfmissingDays,
+      numberOfmissingDaysMax
+    );
+    // collect the missing date(s)
+    let dayNumber = 1;
+    const range = [];
+    while (dayNumber <= maxNumberOfDays) {
+      range[dayNumber - 1] = new Date(AddDaysToDate(lastDate, dayNumber));
+      dayNumber += 1;
+    }
+    // build promise(s)
+    const missingDatesPromises = [];
+    for (let i = 1; i <= maxNumberOfDays; i++) {
+      const baseurl =
+        "https://raw.githubusercontent.com/Rubber1Duck/RD_RKI_COVID19_DATA/master/Fallzahlen/FixFallzahlen_XXXX-XX-XX_LK.json";
+      const url = baseurl.replace(
+        "XXXX-XX-XX",
+        range[i - 1].toISOString().split("T").shift()
+      );
+      missingDatesPromises[i - 1] = axios.get(url).then((response) => {
+        return response.data;
+      });
+    }
+    // request the missing date(s)
+    missingDatesData = await Promise.all(missingDatesPromises);
+    // add the missing date(s) to districts
+    for (let day = 0; day < maxNumberOfDays; day++) {
+      districts = districts.map((district) => {
+        if (missingDatesData[day][district.ags]) {
+          district.history.push({
+            weekIncidence: missingDatesData[day][district.ags].incidence_7d,
+            date: new Date(missingDatesData[day][district.ags].Datenstand),
+          });
+        }
+        return district;
+      });
+    }
   }
 
   // do we need to fetch archive data as well?
@@ -186,8 +253,6 @@ async function getStatesFrozenIncidenceHistoryArchive(): Promise<
   // table starts in row 5 (parameter is zero indexed)
   const json = XLSX.utils.sheet_to_json(sheet, { range: 4 });
 
-  const lastUpdate = new Date(response.headers["last-modified"]);
-
   let states = json.map((states) => {
     const name = states["__EMPTY"]; //there is no header
     const abbreviation = getStateAbbreviationByName(name);
@@ -229,7 +294,7 @@ export async function getStatesFrozenIncidenceHistory(
   // table starts in row 5 (parameter is zero indexed)
   const json = XLSX.utils.sheet_to_json(sheet, { range: 4 });
 
-  const lastUpdate = new Date(response.headers["last-modified"]);
+  let lastUpdate = new Date(response.headers["last-modified"]);
 
   let states = json.map((states) => {
     const name = states["MeldeLandkreisBundesland"];
@@ -246,7 +311,7 @@ export async function getStatesFrozenIncidenceHistory(
       history.push({ weekIncidence: states[dateKey], date });
     });
 
-    if (days != null) {
+    if (days) {
       const reference_date = new Date(getDateBefore(days));
       history = history.filter((element) => element.date > reference_date);
     }
@@ -254,8 +319,72 @@ export async function getStatesFrozenIncidenceHistory(
     return { abbreviation, name, history };
   });
 
-  if (abbreviation != null) {
+  if (abbreviation) {
     states = states.filter((states) => states.abbreviation === abbreviation);
+  }
+
+  // The Excel sheet with fixed incidence data is only updated on mondays
+  // check witch date is the last date in history
+  const lastDate: Date = states[0].history[states[0].history.length - 1].date;
+  const today: Date = new Date(new Date().setHours(0, 0, 0));
+  // get lastUpdate from github
+  const lastGithubDate = await axios
+    .get(
+      `https://raw.githubusercontent.com/Rubber1Duck/RD_RKI_COVID19_DATA/master/Fallzahlen/RKI_COVID19_meta.json`
+    )
+    .then((response) => {
+      return new Date(response.data.modified);
+    });
+  // if lastDate < today and lastDate <= lastGithubDate try to get the missing dates from github
+  // the gihub data is updated by github actions daily, triggert at 1:58 GMT, running ?
+  let missingDatesData = [];
+  if (
+    lastDate.getTime() < today.getTime() &&
+    lastDate.getTime() <= lastGithubDate.getTime()
+  ) {
+    lastUpdate = lastGithubDate;
+    const numberOfmissingDays = getDayDifference(today, lastDate) - 1;
+    const numberOfmissingDaysMax =
+      getDayDifference(lastGithubDate, lastDate) - 1;
+    const maxNumberOfDays = Math.min(
+      numberOfmissingDays,
+      numberOfmissingDaysMax
+    );
+    // collect the missing date(s)
+    let dayNumber = 1;
+    const range = [];
+    while (dayNumber <= maxNumberOfDays) {
+      range[dayNumber - 1] = new Date(AddDaysToDate(lastDate, dayNumber));
+      dayNumber += 1;
+    }
+    // build promise(s)
+    const missingDatesPromises = [];
+    for (let i = 1; i <= maxNumberOfDays; i++) {
+      const baseurl =
+        "https://raw.githubusercontent.com/Rubber1Duck/RD_RKI_COVID19_DATA/master/Fallzahlen/FixFallzahlen_XXXX-XX-XX_BL.json";
+      const url = baseurl.replace(
+        "XXXX-XX-XX",
+        range[i - 1].toISOString().split("T").shift()
+      );
+      missingDatesPromises[i - 1] = axios.get(url).then((response) => {
+        return response.data;
+      });
+    }
+    // request the missing date(s)
+    missingDatesData = await Promise.all(missingDatesPromises);
+    // add the missing date(s) to states
+    for (let day = 0; day < maxNumberOfDays; day++) {
+      states = states.map((state) => {
+        const id = getStateIdByName(state.name)
+          ? getStateIdByName(state.name)
+          : 0;
+        state.history.push({
+          weekIncidence: missingDatesData[day][id].incidence_7d,
+          date: new Date(missingDatesData[day][id].Datenstand),
+        });
+        return state;
+      });
+    }
   }
 
   // do we need to fetch archive data as well?
@@ -275,7 +404,7 @@ export async function getStatesFrozenIncidenceHistory(
       );
     }
     // filter by days
-    if (days != null) {
+    if (days) {
       const reference_date = new Date(getDateBefore(days));
       archiveData = archiveData.map((state) => {
         state.history = state.history.filter(
