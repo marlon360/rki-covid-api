2e1ea85418075dca2ba35f7cbeb40eca038d232e
Peter Markewitz <peter@markewitz.org>
2023-10-23 12:43:46 +0000

    fix_20231023

--- src/data-requests/frozen-incidence.ts
+++ src/data-requests/frozen-incidence.ts
@@ -71,8 +71,8 @@ interface Region {
 const Districts: Region = {
   Actual: {
     type: "ActualDistricts",
-    url: "https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Fallzahlen_Kum_Tab_aktuell.xlsx?__blob=publicationFile",
-    WorkBook: "Official, from Fallzahlen_Kum_Tab_aktuell.xlsx",
+    url: "https://raw.githubusercontent.com/Rubber1Duck/RD_RKI_COVID19_DATA/master/Fallzahlen/Fallzahlen_Kum_Tab_aktuell.xlsx",
+    WorkBook: "Unofficial, copy of Fallzahlen_Kum_Tab_aktuell.xlsx",
     SheetName: "LK_7-Tage-Inzidenz (fixiert)",
     startRow: 4,
     startColumn: 2,
@@ -81,8 +81,8 @@ const Districts: Region = {
   },
   Archive: {
     type: "ArchiveDistricts",
-    url: "https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Fallzahlen_Kum_Tab_Archiv.xlsx?__blob=publicationFile",
-    WorkBook: "Official, from Fallzahlen_Kum_Tab_Archiv.xlsx",
+    url: "https://raw.githubusercontent.com/Rubber1Duck/RD_RKI_COVID19_DATA/master/Fallzahlen/Fallzahlen_Kum_Tab_Archiv.xlsx",
+    WorkBook: "Unofficial, copy of Fallzahlen_Kum_Tab_Archiv.xlsx",
     SheetName: "LK_7-Tage-Inzidenz (fixiert)",
     startRow: 4,
     startColumn: 3,
@@ -100,8 +100,8 @@ const Districts: Region = {
 const States: Region = {
   Actual: {
     type: "ActualStates",
-    url: "https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Fallzahlen_Kum_Tab_aktuell.xlsx?__blob=publicationFile",
-    WorkBook: "Official, from Fallzahlen_Kum_Tab_aktuell.xlsx",
+    url: "https://raw.githubusercontent.com/Rubber1Duck/RD_RKI_COVID19_DATA/master/Fallzahlen/Fallzahlen_Kum_Tab_aktuell.xlsx",
+    WorkBook: "Unofficial, copy of Fallzahlen_Kum_Tab_aktuell.xlsx",
     SheetName: "BL_7-Tage-Inzidenz (fixiert)",
     startRow: 4,
     startColumn: 1,
@@ -110,8 +110,8 @@ const States: Region = {
   },
   Archive: {
     type: "ArchiveStates",
-    url: "https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Fallzahlen_Kum_Tab_Archiv.xlsx?__blob=publicationFile",
-    WorkBook: "Official, from Fallzahlen_Kum_Tab_Archiv.xlsx",
+    url: "https://raw.githubusercontent.com/Rubber1Duck/RD_RKI_COVID19_DATA/master/Fallzahlen/Fallzahlen_Kum_Tab_Archiv.xlsx",
+    WorkBook: "Unofficial, copy of Fallzahlen_Kum_Tab_Archiv.xlsx",
     SheetName: "BL_7-Tage-Inzidenz (fixiert)",
     startRow: 4,
     startColumn: 1,
@@ -157,7 +157,6 @@ const RKIFrozenIncidenceHistoryPromise = async function (resolve, reject) {
       reject(new RKIError(rdata.error, response.config.url));
       throw new RKIError(rdata.error, response.config.url);
     }
-    lastUpdate = new Date(response.headers["last-modified"]);
     const workbook = XLSX.read(rdata, { type: "buffer", cellDates: true });
     const sheet = workbook.Sheets[parameter.SheetName];
     // table starts in row "startRow" (parameter is zero indexed)
@@ -205,7 +204,7 @@ const RKIFrozenIncidenceHistoryPromise = async function (resolve, reject) {
       });
       return { [parameter.key]: regionKey, name: name, history: history };
     });
-
+    lastUpdate = new Date(data[0].history[data[0].history.length - 1].date);
     //prepare data for redis entry for the excelsheet
     const JsonData = JSON.stringify({ lastUpdate, data });
 
